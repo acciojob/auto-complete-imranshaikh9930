5c318dd2262dcad733a524e7cc68e43eb9fb59b8
Imran shaikh <imranpathan7040@gmail.com>
2024-01-21 22:05:39 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React,{useEffect, useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const  fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const[input,setInput] = useState("");
+  const [arr,setArr] = useState([])
+
+ useEffect(()=>{
+
+  const filteredSuggestions = fruits.filter(fruit =>
+    fruit.toLowerCase().includes(input.toLowerCase())
+  );
+  
+  setArr(filteredSuggestions);
+  
+ },[input])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+
+      <h1>Search Item</h1>
+      
+      <input type="text" value={input} onChange={(e)=>setInput(e.target.value)} />
+    {
+      arr.map((item)=>
+      (
+        <ul>
+          <li key={item}>{item}</li>
+        </ul>
+      )
+      )
+    }
     </div>
   )
 }
